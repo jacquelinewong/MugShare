8785e7475730e895333829db9766d954b521cae9
Jacqueline Wong <jacqueline.wong00@gmail.com>
2016-11-08 20:29:15 -0800

    First commit

---
+++ MugShareWebApp/MugShareWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MugShareWebApp.MvcApplication" Language="C#" %>
---
+++ MugShareWebApp/MugShareWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MugShareWebApp.Startup))]
+namespace MugShareWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
